e155253e781b1ebea1700575e1ee7d83e1d8a69f
Anton Shiryaev <med.phisiker@gmail.com>
2025-06-20 20:43:23 +1000

    refactor: migrate prompt handling to dedicated prompt-handler package
    - Migrate load_prompt import from model_interface to prompt_handler
    - Add prepare_prompt import from prompt_handler
    - Add prompt-handler dependency and workspace source
    - Update version to 0.1.2.dev3

--- bench_utils/model_utils.py
+++ bench_utils/model_utils.py
@@ -1,5 +1,6 @@
 """Модуль для работы с моделями (обратная совместимость)."""
-from model_interface.model_factory import ModelFactory, load_prompt  # type: ignore
+from model_interface.model_factory import ModelFactory  # type: ignore
+from prompt_handler import load_prompt, prepare_prompt  # type: ignore
 
 initialize_model = ModelFactory.initialize_model
 create_qwen_model_config = ModelFactory.create_qwen_model_config
@@ -10,5 +11,6 @@ __all__ = [
     "create_qwen_model_config",
     "initialize_qwen_model",
     "load_prompt",
+    "prepare_prompt",
     "ModelFactory",
 ] 
